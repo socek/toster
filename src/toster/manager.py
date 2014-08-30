7d23cec24c354fdd43ef8fe4ef6e02427c24d184
Dominik "Socek" Dlugajczyk <msocek@gmail.com>
2014-08-31 00:02:10 +0200

    Added reading test cases from yaml file.

--- src/toster/manager.py
+++ src/toster/manager.py
@@ -1,3 +1,4 @@
+from yaml import load
 from importlib import import_module
 
 
@@ -12,6 +13,12 @@ class TestManager(object):
         for testcase in testcases:
             self.add_testcase(testcase)
 
+    def add_testcases_from_yaml(self, path):
+        with open(path, 'r') as stream:
+            data = load(stream)
+        for url, cls in self._inner_dict(data):
+            self.add_testcase('%s:%s' % (url, cls))
+
     def add_testcase(self, testcase):
         testcase = self.maybe_dotted(testcase)
         key = testcase.__module__ + ':' + testcase.__name__
@@ -23,6 +30,18 @@ class TestManager(object):
             group = self.groups.get(name, [])
             group.append(testcase)
             self.groups[name] = group
+
+    def _inner_dict(self, data, prefix=''):
+        if data is None:
+            return
+        for name, value in data.items():
+            if value is None:
+                continue
+            elif type(value) is dict:
+                yield from self._inner_dict(value, prefix + name + '.')
+            else:
+                for element in value:
+                    yield (prefix + name, element)
 
     def maybe_dotted(self, testcase):
         if type(testcase) is not str:
